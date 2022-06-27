380be561081e8af35d9626c8dfe2ecae868fc738
andres-mestra <andresmestra11a@gmail.com>
2022-06-27 12:08:25 -0500

    feat: implement ui provider for navigation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/pages/pages.dart';
 import 'package:qr_reader/widgets/widgets.dart';
+import 'package:qr_reader/providers/providers.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -17,12 +20,30 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
-      body: const Center(
-        child: Text('HomePage'),
-      ),
+      body: const _HomePageBody(),
       bottomNavigationBar: const CustomNavigationBar(),
       floatingActionButton: const ScanButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+  const _HomePageBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final uiProvider = Provider.of<UiProvider>(context);
+
+    final currentIndex = uiProvider.selectedMenuOpt;
+
+    switch (currentIndex) {
+      case 0:
+        return const MapasPage();
+      case 1:
+        return const DireccionesPage();
+      default:
+        return const MapasPage();
+    }
   }
 }
--- lib/widgets/custom_navigationbar.dart
+++ lib/widgets/custom_navigationbar.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/providers/providers.dart';
 
 class CustomNavigationBar extends StatelessWidget {
   const CustomNavigationBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final uiProvider = Provider.of<UiProvider>(context);
+
+    final currentIndex = uiProvider.selectedMenuOpt;
+
     return BottomNavigationBar(
-      currentIndex: 1,
+      onTap: (int i) => uiProvider.selectedMenuOpt = i,
+      currentIndex: currentIndex,
       elevation: 0,
       items: const [
         BottomNavigationBarItem(
