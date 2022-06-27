0cfef8fe210b6c3b3d3080033ff880e408fed72d
andres-mestra <andresmestra11a@gmail.com>
2022-06-27 12:05:14 -0500

    feat: define ui provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:qr_reader/pages/pages.dart';
+import 'package:qr_reader/providers/providers.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,16 +10,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'QR reader',
-      initialRoute: 'home',
-      routes: {
-        'home': (_) => const HomePage(),
-        'mapa': (_) => const MapaPage(),
-      },
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UiProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'QR reader',
+        initialRoute: 'home',
+        routes: {
+          'home': (_) => const HomePage(),
+          'mapa': (_) => const MapaPage(),
+        },
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+        ),
       ),
     );
   }
