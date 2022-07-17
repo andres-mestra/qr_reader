b131d0d42c138ccab2e8441d8883f4461cfd3474
andres-mestra <andresmestra11a@gmail.com>
2022-07-17 14:59:38 -0500

    refactor: show scan list

--- lib/pages/mapas_page.dart
+++ lib/pages/mapas_page.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:qr_reader/widgets/list_scans.dart';
-import 'package:qr_reader/hooks/use_providers.dart';
 
 class MapasPage extends StatelessWidget {
   const MapasPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final scanListProvider = UseProviders.scanListProvider(context);
-    final scans = scanListProvider.scans;
-
-    return ListScans(scans: scans, icon: Icons.location_on_outlined);
+    return const ListScans(icon: Icons.location_on_outlined);
   }
 }
--- lib/widgets/list_scans.dart
+++ lib/widgets/list_scans.dart
@@ -1,23 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:qr_reader/models/models.dart';
+import 'package:qr_reader/hooks/use_providers.dart';
 
 class ListScans extends StatelessWidget {
   final IconData icon;
-  final List<ScanModel> scans;
 
-  const ListScans({Key? key, required this.scans, required this.icon})
-      : super(key: key);
+  const ListScans({Key? key, required this.icon}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final scanListProvider = UseProviders.scanListProvider(context);
+    final scans = scanListProvider.scans;
+
     return ListView.builder(
       itemCount: scans.length,
-      itemBuilder: (_, i) => ListTile(
-        title: Text(scans[i].valor),
-        subtitle: Text('ID: ${scans[i].id}'),
-        leading: Icon(icon, color: Theme.of(context).primaryColor),
-        trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.grey),
-        onTap: () => print(scans[i].id),
+      itemBuilder: (_, i) => Dismissible(
+        key: UniqueKey(),
+        background: Container(
+          color: Colors.red,
+        ),
+        onDismissed: (DismissDirection direction) {
+          UseProviders.scanListProvider(context, listen: false)
+              .deleteForId(scans[i].id!);
+        },
+        child: ListTile(
+          title: Text(scans[i].valor),
+          subtitle: Text('ID: ${scans[i].id}'),
+          leading: Icon(icon, color: Theme.of(context).primaryColor),
+          trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+          onTap: () => print(scans[i].id),
+        ),
       ),
     );
   }
