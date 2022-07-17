984368029695f634bdcc48eff25eed39366ddefe
andres-mestra <andresmestra11a@gmail.com>
2022-07-17 12:38:06 -0500

    feat: delete scans of db

--- lib/providers/db_provider.dart
+++ lib/providers/db_provider.dart
@@ -72,6 +72,18 @@ class DBProvider {
     return res!;
   }
 
+  Future<int> deleteScan(int id) async {
+    final db = await database;
+    final res = await db?.delete('Scans', where: 'id = ?', whereArgs: [id]);
+    return res!;
+  }
+
+  Future<int> deleteAllScans() async {
+    final db = await database;
+    final res = await db?.delete('Scans');
+    return res!;
+  }
+
   Future<ScanModel?> getScandById(int id) async {
     final db = await database;
     final res = await db?.query('Scans', where: 'id = ?', whereArgs: [id]);
